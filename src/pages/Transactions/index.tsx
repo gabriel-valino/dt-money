96d6a324366a528e5e54c6ed191389433720ecf4
gabriel <gabrielvalino25@hotmail.com>
2024-08-08 15:18:28 -0300

    component: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions() {
@@ -7,6 +9,30 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 17.400,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/10/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 50,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/10/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
