ff9a5c5f4936cf87fe07ff5bf729bc3be860a38a
gabriel <gabrielvalino25@hotmail.com>
2024-08-09 16:09:49 -0300

    create Transactions Context and format date and price values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,31 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
-interface Transaction {
-  id: number
-  description: string
-  type: 'income' | 'outcome'
-  price: number
-  category: string
-  createdAt: string
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTrasactions () {
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTrasactions()
-  }, [])
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
@@ -40,10 +22,13 @@ export function Transactions() {
               <tr key={transaction.id}>
                <td width="50%">{transaction.description}</td>
                <td>
-                 <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighlight>
                </td>
                <td>{transaction.category}</td>
-               <td>{transaction.createdAt}</td>
+               <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
              </tr>
             ))}
           </tbody>
